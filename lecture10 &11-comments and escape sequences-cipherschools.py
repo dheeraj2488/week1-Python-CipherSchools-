67049b6dd5afcfceac8c0a20125d2c1a1c218894
Dheeraj thakur <115392626+dheeraj2488@users.noreply.github.com>
2022-11-26 15:41:29 +0530

    Add files via upload

---
+++ lecture10 &11-comments and escape sequences-cipherschools.py
@@ -0,0 +1,18 @@
+#we cannot use single quote inside single and double quote inside double quote
+#but we can do this by using escape sequence(using backslash and quote we using)
+print("hello \"world\" world")
+print('i\'m dheeraj thakur')
+
+#for print in a new line (\n)
+print("lineA\nlineB")
+print("lineA\n lineB\n lineC")
+
+#/t is used for tab
+print("\tthakur\tdheeraj\tlearner")
+
+#double backslash used for using single backslash
+print("hello my name is dheeraj\\")
+#four bckslash used for using double backslash
+print("hello my name is dheeraj\\\\")
+#\b will work as backspace
+print("hell\blo world")
