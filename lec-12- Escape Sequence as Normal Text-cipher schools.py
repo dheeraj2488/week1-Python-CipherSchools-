dcc9fcf63af016f0dba2156f845d8cbc86c4037c
Dheeraj thakur <115392626+dheeraj2488@users.noreply.github.com>
2022-11-26 15:41:59 +0530

    Add files via upload

---
+++ lec-12- Escape Sequence as Normal Text-cipher schools.py
@@ -0,0 +1,8 @@
+#if we want output: lineA /n lineB
+print("lineA \\n lineB")
+print("my name \\\\n dheeraj")
+print("cipher \\\\\\\\n schools")
+print("dheraj \ thakur")
+# output: \" \'
+print(" \\\" \\\' ")
+print("my name is \\\"dheeraj\\\" and i want to be a \\\'software engineer\\\'" )
